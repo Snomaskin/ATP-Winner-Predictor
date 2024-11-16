e49a10d1c69414890af6e71958889653c6a56979
Snomaskin <kalleanka@skiff.com>
2024-11-16 14:12:19 +0100

    refactoring

--- www/inputFieldsSetup.js
+++ www/inputFieldsSetup.js
@@ -1,21 +1,21 @@
 /**
- * @returns {Array<Object>} An array of player objects
+ * @returns {Promise<Array<Object>>}
  */
 async function loadPlayerData() {
     const PLAYERS_CSV_URL = "player_index_df.csv";
+
     try {
         const fetchCSV = await fetch(PLAYERS_CSV_URL);
         const csvData = await fetchCSV.text();
-
         return parseCsvToJson(csvData);
     } catch (error) {
         console.error('Error loading player data:', error);
         return null;
-    };
-};
+    }
+}
 
 /**
- * @param {string} csvData - The CSV data to parse
+ * @param {string} csvData
  * @returns {Array<Object>} An array of player objects
  */
 function parseCsvToJson(csvData){
@@ -28,9 +28,9 @@ function parseCsvToJson(csvData){
             Index: parseInt(Index),
             TotalWins: parseInt(TotalWins),
             searchName: Player.toLowerCase()
-        };
+        }
     });
-};
+}
 
 // Handler function for initializing the input fields. It's called when the DOM is loaded.
 async function setupInputFields(){
@@ -44,29 +44,25 @@ async function setupInputFields(){
         const inputField = document.getElementById(fieldId);
         
         const suggestionsContainer = document.createElement('div');
-
         suggestionsContainer.id = `${fieldId}-suggestion`;
-        suggestionsContainer.style.display = 'none';
+        suggestionsContainer.classList.add('suggestions-container');
 
         setupClearInputButton(inputField); // Append clear button to the input field
-
         inputField.insertAdjacentElement('afterend', suggestionsContainer);
-        setupAutoLookup(inputField, suggestionsContainer, playerData)
+        setupPlayerSuggestions(inputField, suggestionsContainer, playerData);
     });
-};
+}
+
 /**
  * @param {HTMLInputElement} inputField - The input field to add the clear button to
  */
 function setupClearInputButton(inputField) {
-    const clearButton = document.createElement('span');
-    clearButton.classList.add('clear-button');
-    clearButton.textContent = 'X';
+    const clearButton = inputField.parentNode.querySelector('.clear-button');
     clearButton.style.display = 'none';
-    inputField.parentNode.appendChild(clearButton); // Append clear button to the input field
-  
+   
     function toggleClearButtonVisibility() {
         clearButton.style.display = inputField.value ? 'flex' : 'none'; // Show clear button if input field has a value
-    };
+    }
   
     inputField.addEventListener('input', toggleClearButtonVisibility);
     inputField.addEventListener('blur', toggleClearButtonVisibility);
@@ -76,7 +72,7 @@ function setupClearInputButton(inputField) {
       inputField.value = '';
       inputField.focus();
     });
-  };
+  }
 
 /**
  * Sets up the auto-lookup functionality for a given input field.
@@ -84,8 +80,8 @@ function setupClearInputButton(inputField) {
  * @param {HTMLDivElement} suggestionsContainer - The container for displaying suggestions
  * @param {Array<Object>} playerData - An array of player objects
  */
-function setupAutoLookup(inputField, suggestionsContainer, playerData) {
-    // Debounce the search to make sure function is only called for the last keystroke
+function setupPlayerSuggestions(inputField, suggestionsContainer, playerData) {
+    // Debounce the search to make sure the function is only called for the last keystroke
     let debounceTimeout;
 
     inputField.addEventListener('input', () => {
@@ -94,55 +90,69 @@ function setupAutoLookup(inputField, suggestionsContainer, playerData) {
         debounceTimeout = setTimeout(() => {
             const searchTerm = inputField.value;
 
-            if (searchTerm.length < 2) { // Prevent search if input is less than 2 characters
+            if (searchTerm.length < 2) {
                 suggestionsContainer.style.display = 'none';
                 return;
-            };
-
-            const matchingPlayers = playerData.filter(player => 
-                player.searchName.includes(searchTerm.toLowerCase()) // searchName is lowercase for case-insensitive search
-            ).slice(0,5); // Find first 5 matching players
-            updatePlayerSuggestions(matchingPlayers, suggestionsContainer, inputField);
+            }
+            
+            const matchingPlayers = matchPlayers(searchTerm, playerData);
+            setupSuggestionsContainer(matchingPlayers, suggestionsContainer, inputField);
         });
     });
-};
+}
+
+function matchPlayers(searchTerm, playerData){
+    return playerData.filter(player => 
+        player.searchName.includes(searchTerm.toLowerCase()) // searchName is lowercase for case-insensitive search
+    ).slice(0,5); // Find first 5 matching players
+}
 
 /**
  * Updates the player suggestions display based on the matching players. 
- * @param {Array<Object>} players - An array of player objects
- * @param {HTMLDivElement} container - The container for displaying suggestions
- * @param {HTMLInputElement} inputField - The input field to monitor for changes
+ * @param {Array<Object>} players
+ * @param {HTMLDivElement} suggestionsContainer
+ * @param {HTMLInputElement} inputField
  */
-function updatePlayerSuggestions(players, suggestionsContainer, inputField){
+function setupSuggestionsContainer(players, suggestionsContainer, inputField){
     if (players.length === 0){
         suggestionsContainer.style.display = 'none';
         return;
-    };
+    }
 
     suggestionsContainer.innerHTML = '';
     const ul = document.createElement('ul');
     ul.className = 'suggestions-list';
 
+    appendPlayerSuggestions(players, inputField, suggestionsContainer, ul);
+
+    suggestionsContainer.appendChild(ul);
+    suggestionsContainer.style.display = 'block';
+}
+
+/**
+* @param {Array<Object>} players
+* @param {HTMLInputElement} inputField 
+* @param {HTMLElement} suggestionsContainer 
+* @param {HTMLUListElement} ul 
+*/
+function appendPlayerSuggestions(players, inputField, suggestionsContainer, ul){
     players.forEach(player => {
         const li = document.createElement('li');
         li.className = 'suggestions-item';
         // Use the correctly capitalized player name for display:
         li.innerHTML = `
-            <div class="player-name">${player.Player}</div>
+            <div class="player-name">${player.Player}<br>
                 Wins: ${player.TotalWins}
             </div>
         `;
         li.addEventListener('click', () => {
             inputField.value = player.Player; // Use the correctly capitalized player name for input
             suggestionsContainer.style.display = 'none';
+            inputField.parentNode.querySelector('.clear-button').style.display = 'flex';
         });
 
         ul.appendChild(li);
     });
-    suggestionsContainer.appendChild(ul);
-    suggestionsContainer.style.display = 'block';
-    suggestionsContainer.style.position = 'absolute';
-    suggestionsContainer.style.width = inputField.offsetWidth + 'px';
-};
+}
 
 document.addEventListener('DOMContentLoaded', setupInputFields);
