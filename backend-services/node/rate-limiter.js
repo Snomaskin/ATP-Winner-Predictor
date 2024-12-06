3bf43c21fa7c0017f68da1e4ecb493c5537039e6
Snomaskin <kalleanka@skiff.com>
2024-12-06 16:49:13 +0100

    NodeCache instead of redis for rate limiter

--- backend-services/node/rate-limiter.js
+++ backend-services/node/rate-limiter.js
@@ -1,21 +1,20 @@
-const redisClient = require('redis').createClient()
-redisClient.connect()
+const NodeCache = require('node-cache')
+const cache = new NodeCache()
 
 const RateLimitConfig = {
     maxRequests: 10,      
-    expiration: 60000,
+    expiration: 60,
 }
     
 async function checkLimit(clientKey, endpoint) {
     const key = `${clientKey}-${endpoint}`;
-    
-    const currentCount = await redisClient.incr(key);
-    if (currentCount === 1){
-        await redisClient.expire(key, RateLimitConfig.expiration / 1000); // Set TTL for first iteration
-    }
+    let currentCount = cache.get(key) || 0;
+
     if (currentCount > RateLimitConfig.maxRequests) {
         throw new Error('Too many requests. Please wait before trying again.');
     }
+
+    cache.set(key, currentCount + 1, RateLimitConfig.expiration)
 }
 
 module.exports = {
